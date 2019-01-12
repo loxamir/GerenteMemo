5a981defc4db7609e173bbe08477f67016ca765f
Marcelo Pickler <loxamir@gmail.com>
2019-01-12 12:15:37 -0200

    fix bug on cash information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
     },
     {
       title: 'Informes',
-      url: '/report-tabs',
+      url: '/report-list',
       icon: 'stats'
     },
     // {
