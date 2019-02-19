d4b4e127ec36d1a142d827c7dd0e61eeaf5ab238
Marcelo Pickler <loxamir@gmail.com>
2019-02-19 11:36:09 -0300

    Fix add missing bank and check types when creating moves inside cashier

--- src/app/cash-move/cash-move.page.ts
+++ src/app/cash-move/cash-move.page.ts
@@ -136,11 +136,11 @@ export class CashMovePage implements OnInit {
       currency_residual: new FormControl(this.currency_residual||0),
       _id: new FormControl(''),
     });
-    if (this.accountTo && this.accountTo['_id'].split('.')[1]=='cash'){
+    if (this.accountTo && (this.accountTo['_id'].split('.')[1]=='cash' || this.accountTo['_id'].split('.')[1]=='bank' || this.accountTo['_id'].split('.')[1]=='check')){
       // console.log("to cash");
       this.to_cash = true;
     }
-    if (this.accountFrom && this.accountFrom['_id'].split('.')[1]=='cash'){
+    if (this.accountFrom && (this.accountFrom['_id'].split('.')[1]=='cash' || this.accountFrom['_id'].split('.')[1]=='bank' || this.accountFrom['_id'].split('.')[1]=='check')){
       // console.log("from cash");
       this.from_cash = true;
     }
