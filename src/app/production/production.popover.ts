9e22c01470483d5450f908782ea30671c12e9ef5
Marcelo Pickler <loxamir@gmail.com>
2019-02-28 13:43:26 -0300

    Fix labels on production popover

--- src/app/production/production.popover.ts
+++ src/app/production/production.popover.ts
@@ -5,13 +5,14 @@ import {  NavController, PopoverController, ToastController, NavParams } from '@
   template: `
   <ion-list>
     <ion-item (click)="duplicate()">Duplicar</ion-item>
-    <ion-item (click)="share()">Compartir</ion-item>
-    <ion-item (click)="cancel()">Cancelar</ion-item>
+    <ion-item (click)="cancel()" *ngIf="navParams.data.doc.productionForm.value.state=='CONFIRMED' && navParams.data.doc.productionForm.value.date.split('T')[0]==today.split('T')[0]">Desconfirmar</ion-item>
+    <ion-item (click)="cancel()" *ngIf="navParams.data.doc.productionForm.value.state=='SCHEDULED' || navParams.data.doc.productionForm.value.state=='STARTED'">Volver a Borrador</ion-item>
   </ion-list>
   `
 })
 export class ProductionPopover {
   pop: PopoverController;
+  today;
 
   constructor(
     public navParams: NavParams,
@@ -19,6 +20,7 @@ export class ProductionPopover {
     public toastCtrl: ToastController,
   ) {
     this.pop = navParams.get('popoverController');
+    this.today = new Date().toISOString();
   }
     async duplicate(){
       this.navParams.data.doc._id = '';
