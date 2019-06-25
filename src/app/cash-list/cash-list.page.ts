6cb96e9eac6462f1aa045f7a72b5c24ba932d04f
Marcelo Pickler <loxamir@gmail.com>
2019-06-25 07:41:59 -0300

    Improve concistency and performance on cash list

--- src/app/cash-list/cash-list.page.ts
+++ src/app/cash-list/cash-list.page.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, ModalController, LoadingController,  Events, PopoverController, AlertController } from '@ionic/angular';
+import { NavController, ModalController, LoadingController, Events, PopoverController, AlertController } from '@ionic/angular';
 import { CashPage } from '../cash/cash.page';
 
 import 'rxjs/Rx';
-// import { CashListService } from './cash-list.service';
 import { CashListPopover } from './cash-list.popover';
-// import { PouchdbService } from '../services/pouchdb/pouchdb-service';
+import { FormatService } from '../services/format.service';
 import { CashService } from '../cash/cash.service';
 import { CashMoveService } from '../cash-move/cash-move.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -23,7 +22,7 @@ export class CashListPage implements OnInit {
   select;
   page = 0;
   currency_precision = 2;
-  user:any = {};
+  user: any = {};
   company_currency_id = 'currency.PYG';
   currencies = {};
   constructor(
@@ -31,6 +30,7 @@ export class CashListPage implements OnInit {
     public loadingCtrl: LoadingController,
     public route: ActivatedRoute,
     public pouchdbService: PouchdbService,
+    public formatService: FormatService,
     public cashService: CashService,
     public events: Events,
     public popoverCtrl: PopoverController,
@@ -38,7 +38,6 @@ export class CashListPage implements OnInit {
     public alertCtrl: AlertController,
     public modalCtrl: ModalController,
   ) {
-    //this.loading = //this.loadingCtrl.create();
     this.select = this.route.snapshot.paramMap.get('select');
     var foo = { foo: true };
     history.pushState(foo, "Anything", " ");
@@ -47,7 +46,7 @@ export class CashListPage implements OnInit {
   async ngOnInit() {
     this.loading = await this.loadingCtrl.create();
     await this.loading.present();
-    let config:any = (await this.pouchdbService.getDoc('config.profile'));
+    let config: any = (await this.pouchdbService.getDoc('config.profile'));
     this.currency_precision = config.currency_precision;
 
     this.company_currency_id = config.currency_id;
@@ -60,17 +59,16 @@ export class CashListPage implements OnInit {
 
     this.user = (await this.pouchdbService.getUser());
     this.setFilteredItems();
-    this.events.subscribe('changed-cash-move', (change)=>{
+    this.events.subscribe('changed-cash-move', (change) => {
       this.handleViewChange(this.cashList, change);
     })
-    this.events.subscribe('refresh-cash-list', (change)=>{
+    this.events.subscribe('refresh-cash-list', (change) => {
       this.handleViewChange(this.cashList, change);
     })
-    this.events.subscribe('changed-account', (change)=>{
-      // this.cashListService.handleChange(this.cashList, change);
+    this.events.subscribe('changed-account', (change) => {
       this.setFilteredItems();
     })
-    this.events.subscribe('got-database', ()=>{
+    this.events.subscribe('got-database', () => {
       this.setFilteredItems();
     })
   }
@@ -89,7 +87,6 @@ export class CashListPage implements OnInit {
     }, 50);
   }
 
-
   doRefresh(refresher) {
     setTimeout(() => {
       this.setFilteredItems();
@@ -107,19 +104,11 @@ export class CashListPage implements OnInit {
     });
   }
 
-  // presentPopover(myEvent) {
-  //   let popover = this.popoverCtrl.create(CashListPopover);
-  //   popover.present({
-  //     ev: myEvent
-  //   });
-  // }
-
   async presentPopover(myEvent) {
-    console.log("teste my event");
     let popover = await this.popoverCtrl.create({
       component: CashListPopover,
       event: myEvent,
-      componentProps: {popoverController: this.popoverCtrl}
+      componentProps: { popoverController: this.popoverCtrl }
     });
     popover.present();
   }
@@ -128,34 +117,19 @@ export class CashListPage implements OnInit {
     this.events.subscribe('open-cash', (data) => {
       this.events.unsubscribe('open-cash');
     })
-    // let newRootNav = <NavController>this.app.getRootNavById('n4');
-    // newRootNav.push(CashPage, {'_id': cash._id});
-    this.navCtrl.navigateForward(['/cash', {'_id': cash._id}]);
+    this.navCtrl.navigateForward(['/cash', { '_id': cash._id }]);
   }
 
   selectCash(cash) {
-    if (this.select){
-      // this.navCtrl.navigateBack().then(() => {
-        this.events.publish('select-cash', cash);
-        this.modalCtrl.dismiss();
-      // });
+    if (this.select) {
+      this.events.publish('select-cash', cash);
+      this.modalCtrl.dismiss();
     } else {
       this.openCash(cash);
     }
   }
 
   async createCash() {
-    // this.events.subscribe('create-cash', (data) => {
-    //   if (this.select){
-    //     this.navCtrl.navigateBack().then(() => {
-    //       this.events.publish('select-cash', data);
-    //     });
-    //   }
-    //   this.events.unsubscribe('create-cash');
-    // })
-    // let newRootNav = <NavController>this.app.getRootNavById('n4');
-    // newRootNav.push(CashPage, {});
-
     let prompt = await this.alertCtrl.create({
       header: 'Crear Caja',
       message: 'Nombre del Caja?',
@@ -176,21 +150,21 @@ export class CashListPage implements OnInit {
             let prompt = await this.alertCtrl.create({
               header: 'Tipo de Caja',
               message: 'Tipo del Caja?',
-              inputs:  [
+              inputs: [
                 {
-                    type:'radio',
-                    label:'Caixa',
-                    value:'cash'
+                  type: 'radio',
+                  label: 'Caixa',
+                  value: 'cash'
                 },
                 {
-                    type:'radio',
-                    label:'Banco',
-                    value:'bank'
+                  type: 'radio',
+                  label: 'Banco',
+                  value: 'bank'
                 },
                 {
-                    type:'radio',
-                    label:'Cheque',
-                    value:'check'
+                  type: 'radio',
+                  label: 'Cheque',
+                  value: 'check'
                 }
               ],
               buttons: [
@@ -207,7 +181,7 @@ export class CashListPage implements OnInit {
                       'transfer': true,
                       'payable': false,
                       'receivable': false,
-                    }).then((res:any)=>{
+                    }).then((res: any) => {
                       this.setInitialBalance(res.cash);
                     })
                   }
@@ -215,19 +189,6 @@ export class CashListPage implements OnInit {
               ]
             });
             prompt.present();
-
-
-            //
-            // this.cashService.createCash({
-            //   'name': data.name,
-            //   'type': 'liquidity',
-            //   'category_id': 'accountCategory.cash',
-            //   'transfer': true,
-            //   'payable': false,
-            //   'receivable': false,
-            // }).then((res:any)=>{
-            //   this.setInitialBalance(res.cash);
-            // })
           }
         }
       ]
@@ -235,11 +196,11 @@ export class CashListPage implements OnInit {
     prompt.present();
   }
 
-  async setInitialBalance(cash){
+  async setInitialBalance(cash) {
     let self = this;
     let prompt = await this.alertCtrl.create({
       header: 'Balance del Caja',
-      message: 'Cuanto dinero tenes en la Caja '+cash.name+'?',
+      message: 'Cuanto dinero tenes en la Caja ' + cash.name + '?',
       inputs: [
         {
           type: 'number',
@@ -271,85 +232,80 @@ export class CashListPage implements OnInit {
     prompt.present();
   }
 
-  deleteCash(cash){
+  deleteCash(cash) {
     let index = this.cashList.indexOf(cash);
-    if (cash.balance == 0){
+    if (cash.balance == 0) {
       this.cashList.splice(index, 1);
       this.deleteCash2(cash);
     }
   }
 
-  getCashList(keyword, page){
-    return new Promise((resolve, reject)=>{
-      let payableList = [];
-      this.pouchdbService.getView(
-        'stock/Caixas', 1,
-        ['0'],
-        ['z']
-      ).then((planneds: any[]) => {
-        let cashiers = [];
-        this.pouchdbService.searchDocTypeData(
-          'account', keyword
-        ).then((cashList: any[]) => {
-          cashList.forEach(async cashier=>{
-            if (this.user['admin']){
-              if (cashier._id.split('.')[1] == 'cash' || cashier._id.split('.')[1] == 'check' || cashier._id.split('.')[1] == 'bank'){
-                if (cashier.currency_id){
-                  let balance = await this.pouchdbService.getView(
-                    'stock/CaixasForeing', 1, [cashier._id, null], [cashier._id, "z"]);
-                  cashier.balance = balance[0] && balance[0].value || 0;
-                } else {
-                  let balance = await this.pouchdbService.getView(
-                    'stock/Caixas', 1, [cashier._id, null], [cashier._id, "z"]);
-                    cashier.balance = balance[0] && balance[0].value || 0;
-                }
-                cashiers.push(cashier);
-              }
+  getCashList(keyword, page) {
+    return new Promise(async (resolve, reject) => {
+      let cashiers = [];
+      let cashList: any = await this.pouchdbService.searchDocTypeData(
+        'account', keyword
+      );
+      await this.formatService.asyncForEach(cashList, async (cashier: any) => {
+        if (this.user['admin']) {
+          if (cashier._id.split('.')[1] == 'cash'
+            || cashier._id.split('.')[1] == 'check'
+            || cashier._id.split('.')[1] == 'bank') {
+            if (cashier.currency_id) {
+              let balance = await this.pouchdbService.getView(
+                'stock/CaixasForeing', 1, [cashier._id, null], [cashier._id, "z"]);
+              cashier.balance = balance[0] && balance[0].value || 0;
             } else {
-              if (cashier.users && cashier.users.indexOf(this.user['username'])>=0){
-                cashier.balance = 0;
-                if (cashier._id.split('.')[1] == 'cash' || cashier._id.split('.')[1] == 'check' || cashier._id.split('.')[1] == 'bank'){
-                  if (cashier.currency_id){
-                    let balance = await this.pouchdbService.getView(
-                      'stock/CaixasForeing', 1, [cashier._id, null], [cashier._id, "z"]);
-                      cashier.balance = balance[0] && balance[0].value || 0;
-                  } else {
-
-                    let balance = await this.pouchdbService.getView(
-                      'stock/Caixas', 1, [cashier._id, null], [cashier._id, "z"]);
-                      cashier.balance = balance[0] && balance[0].value || 0;
-                  }
-                  cashiers.push(cashier);
-                }
-              }
+              let balance = await this.pouchdbService.getView(
+                'stock/Caixas', 1, [cashier._id, null], [cashier._id, "z"]);
+              cashier.balance = balance[0] && balance[0].value || 0;
             }
-          })
-          resolve(cashiers);
-        });
-      });
+            cashiers.push(cashier);
+          }
+        } else {
+          if (cashier.users && cashier.users.indexOf(this.user['username']) >= 0) {
+            cashier.balance = 0;
+            if (cashier._id.split('.')[1] == 'cash'
+              || cashier._id.split('.')[1] == 'check'
+              || cashier._id.split('.')[1] == 'bank') {
+              if (cashier.currency_id) {
+                let balance = await this.pouchdbService.getView(
+                  'stock/CaixasForeing', 1, [cashier._id, null], [cashier._id, "z"]);
+                cashier.balance = balance[0] && balance[0].value || 0;
+              } else {
+                let balance = await this.pouchdbService.getView(
+                  'stock/Caixas', 1, [cashier._id, null], [cashier._id, "z"]);
+                cashier.balance = balance[0] && balance[0].value || 0;
+              }
+              cashiers.push(cashier);
+            }
+          }
+        }
+      })
+      resolve(cashiers);
     });
   }
 
-  handleChange(list, change){
+  handleChange(list, change) {
     this.pouchdbService.localHandleChangeData(list, change)
   }
 
-  handleViewChange(list, change){
-    if (change.doc.currency_id){
+  handleViewChange(list, change) {
+    if (change.doc.currency_id) {
       this.pouchdbService.getView(
         'stock/CaixasForeing', 1,
         ['0'],
         ['z']
       ).then((cashiers: any[]) => {
         let cashierDict = {}
-        cashiers.forEach(item=>{
+        cashiers.forEach(item => {
           cashierDict[item.key[0]] = item.value;
         })
-        list.forEach((cashier, index)=>{
+        list.forEach((cashier, index) => {
           if (
             change.doc.accountFrom_id == cashier._id
             || change.doc.accountTo_id == cashier._id
-          ){
+          ) {
             cashier.balance = cashierDict[cashier._id] || 0;
           }
         })
@@ -361,14 +317,14 @@ export class CashListPage implements OnInit {
         ['z']
       ).then((cashiers: any[]) => {
         let cashierDict = {}
-        cashiers.forEach(item=>{
+        cashiers.forEach(item => {
           cashierDict[item.key[0]] = item.value;
         })
-        list.forEach((cashier, index)=>{
+        list.forEach((cashier, index) => {
           if (
             change.doc.accountFrom_id == cashier._id
             || change.doc.accountTo_id == cashier._id
-          ){
+          ) {
             cashier.balance = cashierDict[cashier._id] || 0;
           }
         })
